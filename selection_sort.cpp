751c3c555616469856ec109444dfec4821470b43
Maksymilian341 <maksymiliangorny2006@gmail.com>
2026-03-12 17:17:48 +0100

    selection

---
+++ selection_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+    {
+    const int N = 10;
+    int T[N] = {10,5,4,2,3,1,6,7,8,9};
+    int min = 0;
+    for(int i = 0;i < N - 1;i++)
+    {
+        min = i;
+        for(int j = i + 1; j < N;j++)
+        {
+            if(T[j] < T[min])
+            {
+                min = j;
+            }
+        }
+        int temp = T[min];
+        T[min] = T[i];
+        T[i] = temp;
+    }
+    for(int i = 0;i < N;i++)
+    {
+        cout << T[i] << " ";
+    }
+    cout << endl; 
+    return 0;
+    }
