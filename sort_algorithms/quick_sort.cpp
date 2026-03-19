cc3989a22f77ecea270c37ece380bbafbe936252
Maksymilian341 <maksymiliangorny2006@gmail.com>
2026-03-19 14:49:27 +0100

    quick sort

---
+++ sort_algorithms/quick_sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+int partition(std::vector<int>& T,int p, int r)
+{
+    int x = T[r];
+    int i = p;
+    int j = r;
+    while(true)
+    {
+        while(T[i] < x)
+        {
+            i++;
+        }
+        while(T[j] > x)
+        {
+            j--;
+        }
+        if(i <= j)
+        {
+            std::swap(T[i], T[j]);
+            i++;
+            j--;
+        }
+        else
+            return j;
+    }
+}
+void quick_sort(std::vector<int>& T, int p, int r)
+{
+    if(p < r)
+    {
+    int q = partition(T,p,r);
+    quick_sort(T,p,q);
+    quick_sort(T,q + 1,r);
+    }
+}
+int main()
+{
+    std::vector<int> T = {3,4,5,2,1,6,7,9,10,8};
+    int p = 0;
+    int r = T.size() - 1;
+    quick_sort(T,p,r);
+    for(int i = 0;i < T.size();i++)
+        std::cout << T[i] << " ";
+    std::cout << std::endl; 
+    return 0;
+}
