70a5a06b0d1b567b94bc70409c82298677f87203
Maksymilian341 <maksymiliangorny2006@gmail.com>
2026-03-19 10:18:16 +0100

    insertion sort

--- sort_algorithms/insertion_sort.cpp
+++ sort_algorithms/insertion_sort.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main()
-    {
-    const int N = 10;
-    int T[N] = {10,5,4,2,3,1,6,7,8,9};
+void insertion_sort(std::vector<int>& T)
+{
     int key = 0;
     int i = 0;
-    for(int j = 1;j < N;j++)
+    for(int j = 1;j < T.size();j++)
     {
         key = T[j];
         i = j - 1;
@@ -18,10 +16,13 @@ int main()
         }
         T[i + 1]=key;
     }
-    for(int i = 0;i < N;i++)
+}
+int main()
     {
+    std::vector<int> T = {10,5,4,2,3,1,6,7,8,9};
+    insertion_sort(T);
+    for(int i = 0;i < T.size();i++)
         std::cout << T[i] << " ";
-    }
     std::cout << std::endl; 
     return 0;
     }
